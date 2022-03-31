b91ec2a214f17cbc897c5a8ecaeae1430b6010ea
cep11p <cep11p@gmail.com>
2022-03-31 13:53:35 -0300

    Se limpian parametros inutiles

--- config/params.php
+++ config/params.php
@@ -9,6 +9,4 @@ return [
     'servicioRegistral'=> getenv('SERVICIO_REGISTRAL')?getenv('SERVICIO_REGISTRAL'):'app\components\DummyServicioRegistral',
     'servicioLugar'=> getenv('SERVICIO_LUGAR')?getenv('SERVICIO_LUGAR'):'app\components\DummyServicioLugar',
     'servicioInventario'=> getenv('SERVICIO_INVENTARIO')?getenv('SERVICIO_INVENTARIO'):'app\components\DummyServicioInventario',
-    'USERID_APP'=>'1',
-    'USER_APP'=>'admin',
 ];
